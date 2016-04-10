bb69ebdb64547bb588885293b77298228d21d89f
Gus Smith <hfs5022@psu.edu>
2016-04-09 23:05:19 -0400

    updates constructor to be more flexible (for Update)

--- Update.py
+++ Update.py
@@ -1,7 +1,14 @@
+from operator import setitem
+
 class Update:
 	
-	def __init__(self, update):
-		self.update = update
+	def __init__(self, update = None, field = None, value_expression = None):
+		if update != None:
+			self.update = update
+		elif field is not None and value_expression is not None:
+			self.update = lambda _fields: setitem(_fields, field, value_expression())
+		else:
+			self.update = lambda _fields: None
 		
 	def execute(self, fields):
 		self.update(fields)
