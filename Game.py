e233960646d6b69c5506e843b66bb63457908e36
Gus Smith <hfs5022@psu.edu>
2016-04-09 22:12:05 -0400

    finishes simple working example of a guess the number game

--- Game.py
+++ Game.py
@@ -1,15 +1,7 @@
 class Game:
-	fields = {}
-	states = {}
-	current_state = 0
-	
-	def __init__(self, fields, states, initial_state):
-		# DEFINE FIELDS
-		# SET INITIAL FIELDS
+	def __init__(self, fields, states):
 		self.fields = fields
-		# DEFINE STATES
-		# SET INITIAL STATE
-		self.current_state = self.states[initial_state]
-						
+		self.states = states
+		
 	def step(self):
-		self.current_state = self.current_state.step()
+		self.states[self.fields['current_state']].step(self.fields)
--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from Game import Game
 from State import State
 from Update import Update
+import operator
 
 fields = {
 	'number_to_guess' : 5, 
@@ -11,10 +12,19 @@ fields = {
 
 def main():
 	update_guessed_num = Update(get_guess)
+	update_victory = Update(lambda _fields: operator.setitem(_fields, 'victory', _fields['number_to_guess'] == _fields['guessed_num']))
+	state_guess_number = State([update_guessed_num, update_victory])
+	
+	game = Game(fields, {"choose":state_guess_number})
+	
+	while(not fields['victory']):
+		game.step()
+		
+	print("You won!")
 
 def get_guess(_fields):
 	try:
-		guess = int(raw_input('Guess a number:'))
+		guess = int(input('Guess a number: '))
 		_fields['guessed_num'] = guess
 		return True
 	except ValueError:
